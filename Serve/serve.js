4e61d849fc061a54e5d71f38e7e4af3e4e579f28
Bci3000 <162375004+Bci3000@users.noreply.github.com>
2025-05-13 19:43:28 -0300

    t

--- Serve/serve.js
+++ Serve/serve.js
@@ -1,4 +1,15 @@
 require("colors")
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+
+const uri = 'mongodb+srv://bcitec1:<M@0zinha>@myclusterf.vcakwqd.mongodb.net/?retryWrites=true&w=majority&appName=MyClusterF';
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var dbo = client.db("exemplo_bd");
+var usuarios = dbo.collection("usuarios");
 
 console.log("hola".rainbow)
 
@@ -17,3 +28,32 @@ var server = http.createServer(app);
 server.listen(3000);
 // mensagem exibida no console para debug
 console.log("servido rodando mapa...");
+
+app.post("/cadastrar_usuario", function(req, resp) {
+    var data = { db_nome: req.body.nome, db_login: req.body.login, db_senha: req.body.senha };
+
+    usuarios.insertOne(data, function (err) {
+      if (err) {
+        resp.render('resposta_usuario', {resposta: "Erro ao cadastrar usuário!"})
+      }else {
+        resp.render('resposta_usuario', {resposta: "Usuário cadastrado com sucesso!"})        
+      };
+    });
+   
+});
+app.post("/logar_usuario", function(req, resp) {
+    var data = {db_login: req.body.login, db_senha: req.body.senha };
+
+    usuarios.find(data).toArray(function(err, items) {
+      console.log(items);
+      if (items.length == 0) {
+        resp.render('resposta_usuario', {resposta: "Usuário/senha não encontrado!"})
+      }else if (err) {
+        resp.render('resposta_usuario', {resposta: "Erro ao logar usuário!"})
+      }else {
+        resp.render('resposta_usuario', {resposta: "Usuário logado com sucesso!"})        
+      };
+
+    });
+
+});
